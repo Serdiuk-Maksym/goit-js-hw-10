ff30917dac6a599df93f82f457353e9588609d5a
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-06-19 18:37:43 +0300

    fixing some problems

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,112 @@
-import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import 'slim-select/dist/slimselect.css';
+import SlimSelect from 'slim-select';
 
-import { fetchBreeds } from './api/cat-api';
-import { createMarkup } from './markup/markup';
-import { selectEl } from './refs/refs';
-import { addMarkup } from './utils/utls';
-import { fetchCatByBreed } from './api/cat-api';
-import { divCatEl } from './refs/refs';
-import { createCatMarkup } from './markup/markup';
-import { loadEl } from './refs/refs';
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const catInfo = document.querySelector('.cat-info');
 
-window.addEventListener('load', onLoad);
+// Приховати select.breed-select та div.cat-info, показати p.loader
+breedSelect.style.display = 'none';
+catInfo.style.display = 'none';
+loader.style.display = 'block';
 
-function onLoad() {
-  fetchBreeds()
-    .then(respone => {
-      const markupOption = createMarkup(respone);
-
-      addMarkup(selectEl, markupOption);
-      new SlimSelect({
-        select: '#selectElement',
-      });
-    })
-    .catch(error => console.log(error.message));
-}
-
-selectEl.addEventListener('change', onChange);
-
-function onChange(event) {
-  const id = event.target.value;
-  loadEl.classList.add('active');
-  divCatEl.classList.remove('active');
-  fetchCatByBreed(id)
-    .then(response => {
-      const catInfo = response[0];
-      const cat = createCatMarkup(catInfo);
-      divCatEl.innerHTML = cat;
-      divCatEl.classList.add('active');
-      loadEl.classList.remove('active');
-    })
-    .catch(() => {
-      Notiflix.Notify.failure(
-        'Oops!!! There is no cat!!! &#128008 Maybe he was scared by an angry dog!!! &#128021',
-        {
-          width: '500px',
-          timeout: '5000',
-          fontSize: '25px',
-          opacity: 0.7,
-        }
-      );
-      loadEl.classList.remove('active');
+// Код, що виконується при завантаженні сторінки
+fetchBreeds()
+  .then(breeds => {
+    breeds.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
     });
-}
+
+    // Приховати p.loader, показати select.breed-select
+    breedSelect.style.display = 'block';
+    loader.style.display = 'none';
+
+    breedSelect.addEventListener('change', () => {
+      const selectedBreedId = breedSelect.value;
+
+      // Приховати div.cat-info, показати p.loader
+      catInfo.style.display = 'none';
+      loader.style.display = 'block';
+
+      fetchCatByBreed(selectedBreedId)
+        .then(cat => {
+          catInfo.innerHTML = ''; // Очистити вміст блоку перед додаванням нової інформації
+
+          const image = document.createElement('img');
+          image.src = cat.url;
+          image.classList.add('cat_img');
+          image.setAttribute('loading', 'lazy');
+          catInfo.appendChild(image);
+
+          const breedInfo = document.createElement('div');
+          breedInfo.classList.add('breed-info');
+
+          const breedName = document.createElement('p');
+          breedName.classList.add('title');
+
+          const breedText = document.createTextNode(`Breed: `);
+          breedName.appendChild(breedText);
+
+          const breedSpan = document.createElement('span');
+          breedSpan.textContent = cat.breeds[0].name;
+          breedSpan.classList.add('text');
+          breedName.appendChild(breedSpan);
+
+          breedInfo.appendChild(breedName);
+
+          const description = document.createElement('p');
+          description.classList.add('title');
+
+          const descriptionText = document.createTextNode(`Description: `);
+          description.appendChild(descriptionText);
+
+          const descriptionSpan = document.createElement('span');
+          descriptionSpan.textContent = cat.breeds[0].description;
+          descriptionSpan.classList.add('text');
+          description.appendChild(descriptionSpan);
+
+          breedInfo.appendChild(description);
+
+          const temperament = document.createElement('p');
+          temperament.classList.add('title');
+
+          const temperamentText = document.createTextNode(`Temperament: `);
+          temperament.appendChild(temperamentText);
+
+          const temperamentSpan = document.createElement('span');
+          temperamentSpan.textContent = cat.breeds[0].temperament;
+          temperamentSpan.classList.add('text');
+          temperament.appendChild(temperamentSpan);
+
+          breedInfo.appendChild(temperament);
+
+          catInfo.appendChild(breedInfo);
+
+          // Приховати p.loader, показати div.cat-info
+          catInfo.style.display = 'block';
+          loader.style.display = 'none';
+        })
+        .catch(error => {
+          const errorElement = document.querySelector('.error');
+          errorElement.style.display = 'block';
+          console.error(error);
+        });
+
+      // Ініціалізувати SlimSelect на елементі .breed-select
+      const select = new SlimSelect({
+        select: breedSelect,
+        settings: {
+          maxValuesShown: 5,
+        },
+      });
+    });
+  })
+  .catch(error => {
+    const errorElement = document.querySelector('.error');
+    errorElement.style.display = 'block';
+    console.error();
+  });
